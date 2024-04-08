fea15dc5404e7491c42bc7549bee9e02ef6551f5
NBDS <165669546+nbds1423@users.noreply.github.com>
2024-04-08 13:41:53 -0300

    Add files via upload

--- src/components/embed/embed.rs
+++ src/components/embed/embed.rs
@@ -1,1 +1,10 @@
+use serenity::all::{CreateEmbed, CreateEmbedFooter};
 
+pub fn embed() -> CreateEmbed {
+    return CreateEmbed::default()
+        .title("Ragnarok - Classe")
+        .description("Selecione um cargo baseado na classe do seu personagem principal.")
+        .footer(CreateEmbedFooter::new(
+            "Selecione o Cargo novamente para remover.",
+        ));
+}
