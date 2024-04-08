5ce7b060244f88667ef905c1ceecaa301cfebf90
NBDS <165669546+nbds1423@users.noreply.github.com>
2024-04-08 13:40:42 -0300

    Add files via upload

--- src/cmds/roles.rs
+++ src/cmds/roles.rs
@@ -1,1 +1,25 @@
+use serenity::all::CreateMessage;
 
+use crate::components::embed::embed::embed;
+use crate::components::select_menu::select_roles::select_roles;
+use crate::handler::Command;
+
+pub async fn roles(content: Command) {
+    let Command { message, ctx, .. } = content;
+
+    message
+        .delete(&ctx)
+        .await
+        .expect("[Command - Roles (Delete)] -> ");
+
+    message
+        .channel_id
+        .send_message(
+            &ctx.http,
+            CreateMessage::default()
+                .embed(embed())
+                .components(vec![select_roles()]),
+        )
+        .await
+        .expect("[Command - Roles] -> ");
+}
